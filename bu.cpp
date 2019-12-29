7aa52d7ba8a711782925178c199b6466ed88a5b9
phuongabu141 <56481714+phuongabu141@users.noreply.github.com>
2019-12-29 21:04:19 +0700

    bu.cpp

---
+++ bu.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	char data[50];
+	int counter;
+};
+
+void themHoacDem(char *word, struct Node *words, int *size)
+{
+	int i;
+	bool tonTai = false;
+	for(i = 0; i < *size; i++)
+        {
+		if(strcmp(word, words[i].data) == 0)
+		{
+			words[i].counter++;
+			tonTai = true;
+			break;
+		}
+	}
+	if(!tonTai)
+	{
+		strcpy(words[*size].data, word);
+		words[*size].counter = 1;
+		(*size) ++;
+	}
+}
+
+void tachVaDem(char *s, struct Node *words, int *size) {
+	char *p;
+	const char* delim = " \t\n,.?!;:";
+	for(p = strtok(s, delim); p != NULL; p = strtok(NULL, delim)) {
+		themHoacDem(p, words, size);
+	}
+}
+
+void hienThi(struct Node* words, int size) {
+	int i;
+	for(i = 0; i < size; i++)
+    {
+		cout<< words[i].data<< ": "<< words[i].counter<<endl;
+	}
+}
+
+int main(){
+	char s[1000];
+//fgets(s, 999, stdin);
+  cin.getline(s,1000);
+	struct Node words[200];
+	int size = 0;
+
+	tachVaDem(s, words, &size);
+	hienThi(words, size);
+
+	return 0;
+}
